18e0510ae46d89b75cee2b9b6be8486efa684264
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2021-10-01 14:39:42 +0200

    Allow the option to filter the csv

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import requests
 import pandas as pd
 
+# Request of user whether to write the full amount of data or not
+keep_all_input = input("Do you want to write the full data (this includes a lot of OpenSea metadata)? (yes | no )"
+                       "Press enter to keep the default (no)")
+keep_all = False
+if keep_all_input == "yes":
+    keep_all = True
+
 crew_contract_address = '0x746db7b1728af413c4e2b98216c6171b2fc9d00e'
 # We first need to a get a single adddress that owns a crew member to do the next query
 single_asset_request_url = f"https://api.opensea.io/api/v1/asset/{crew_contract_address}/1/"
@@ -20,6 +27,7 @@ print(f"Retrieving {crew_count} total crew members")
 # Collect crew members
 crew_list = []
 iteration_counter = 0
+
 for i in range(int(crew_count / 50) + 1):
     print(f"Collecting crew {iteration_counter * 50}/{crew_count}")
     multi_asset_request_url = f"https://api.opensea.io/api/v1/assets"
@@ -34,4 +42,7 @@ for i in range(int(crew_count / 50) + 1):
 # Write to csv
 print(f"Writing to csv")
 df = pd.json_normalize(crew_list)
+if not keep_all:
+    df = df.filter(axis=1,
+                   items=['id', 'token_id', 'image_original_url', 'name', 'description', 'traits', 'owner.address'])
 df.to_csv("crew.csv")
